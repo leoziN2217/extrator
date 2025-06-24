a1001669806b210168c344e845c7e78d9d8171dc
leoziN2217 <leonardo.fernandes.silva22@escola.pr.gov.br>
2025-06-24 08:34:19 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+const botaoMostraPalavra = document.querySelector('#botao-palavrachave');
+
+botaoMostraPalavra.addEventListener('click',mostraPalavra)
+
+function mostraPalavra(){
+    alert('fui clicado');
+}
